85a0b5f848b0bd15795b35d60ee46f0d97836c30
urmommine <aljaprant12@gmail.com>
2026-03-07 23:27:21 +0700

    reduce toast timeout

--- config/laravel-toaster-magic.php
+++ config/laravel-toaster-magic.php
@@ -5,9 +5,9 @@ return [
         "closeButton" => true,
         "positionClass" => "toast-top-end",
         "preventDuplicates" => false,
-        "showDuration" => "300",
-        "timeOut" => "3000",
-        "theme" => "default", // Available themes: default, material, ios, glassmorphism, neon, minimal, neumorphism
+        "showDuration" => "100",
+        "timeOut" => "1000",
+        "theme" => "glassmorphism", // Available themes: default, material, ios, glassmorphism, neon, minimal, neumorphism
         "gradient_enable" => true, // Available for: default, material, ios, glassmorphism, neon themes
         "color_mode" => true // Color mode (true or false)
     ],
